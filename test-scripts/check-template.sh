ea5a34e796365ac2ec39450b622b39e83a2389a0
Jim Hague <jim@sinodun.com>
2019-01-03 15:01:44 +0000

    Revisit template test geoip. Use MaxMind test files.
    The City and ASN test files - from https://github.com/maxmind/MaxMind-DB.git - are tiny, and so can be reasonably checked into the repo. I've renamed them from the originals (GeoIP-City-Test.mmdb and GeoLite2-ASN-Test.mmdb) to match the names inspector expects.
    
    Revise the test to use these files. This solves the problems of actual lookups changing over time and perturbing the test.

--- test-scripts/check-template.sh
+++ test-scripts/check-template.sh
@@ -12,19 +12,11 @@ INSP=./inspector
 command -v diff > /dev/null 2>&1 || { echo "No diff, skipping test." >&2; exit 77; }
 command -v head > /dev/null 2>&1 || { echo "No head, skipping test." >&2; exit 77; }
 
-GEOASN=$GEOIPDIR/GeoLite2-ASN.mmdb
-GEOLOC=$GEOIPDIR/GeoLite2-City.mmdb
-
-if [ ! \( -r $GEOASN -a -r $GEOLOC \) ]; then
-    echo "No geo data installed in $GEOIPDIR, skipping test"
-    exit 77
-fi
-
 tmpdir=`mktemp -d -t "check-template.XXXXXX"`
 
 cleanup()
 {
-#    rm -rf $tmpdir
+    rm -rf $tmpdir
     exit $1
 }
 
@@ -50,7 +42,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Template output, first 100 lines.
-$INSP -o - -B template -t $FMT --value node=42 $tmpdir/gold.cdns > $tmpdir/gold.dump.full
+$INSP -o - -B template -g . -t $FMT --value node=42 $tmpdir/gold.cdns > $tmpdir/gold.dump.full
 if [ $? -ne 0 ]; then
     error "dumper failed"
 fi
@@ -59,16 +51,5 @@ if [ $? -ne 0 ]; then
     error "head failed"
 fi
 
-# Now, we have a problem. The gold output is a nsd-live.dump. But as the
-# MaxMind database gets updated, some GeoIP data in the dump may change.
-# Generally, though, this shouldn't be by too much over a reasonable
-# period of time, so for now let's just diff the two and see if the
-# number of lines different exceeds a threshold. There are 100 lines in
-# nsd-live.dump, so let's set the threshold at 25%.
-diff -y -W 320 --suppress-common-lines $tmpdir/gold.dump nsd-live.dump > $tmpdir/dump.diff
-ndiff=$(wc -l $tmpdir/dump.diff | sed -e "s/ .*//")
-if [ $ndiff -gt 25 ]; then
-    error "Template output differs"
-fi
-
-cleanup 0
+diff -q $tmpdir/gold.dump nsd-live.dump
+cleanup $?
