446ba50b9649cc3dc87af7914b5f0af3325027b0
Jim Hague <jim@sinodun.com>
2021-03-17 10:04:37 +0000

    Add test of reading through Unix socket to DNSTAP test.

--- test-scripts/check-dnstap.sh
+++ test-scripts/check-dnstap.sh
@@ -12,6 +12,7 @@ OUTPUTFILE=$srcdir/test-scripts/test.dnstap.debug-dns
 
 command -v mktemp > /dev/null 2>&1 || { echo "No mktemp, skipping test." >&2; exit 77; }
 command -v diff > /dev/null 2>&1 || { echo "No diff, skipping test." >&2; exit 77; }
+command -v netcat > /dev/null 2>&1 || { echo "No netcat, skipping test." >&2; exit 77; }
 
 tmpdir=`mktemp -d -t "check-dnstap.XXXXXX"`
 
@@ -23,7 +24,7 @@ cleanup()
 
 trap "cleanup 1" HUP INT TERM
 
-# Run the compactor with debug-dns and verify it succeeds
+# Run the compactor with debug-dns, read from file, and verify it succeeds
 # and produces the expected output.
 $COMP -c /dev/null --debug-dns -n all --dnstap on $DATAFILE > $tmpdir/debug.out
 if [ $? -ne 0 ]; then
@@ -31,4 +32,20 @@ if [ $? -ne 0 ]; then
 fi
 
 diff -i $OUTPUTFILE $tmpdir/debug.out
+if [ $? -ne 0 ]; then
+    cleanup 1
+fi
+
+# Run the compactor with debug-dns, read from socket, and verify it
+# produces the expected output.
+$COMP -c /dev/null --debug-dns -n all --dnstap-socket $tmpdir/dnstap.sock > $tmpdir/debug2.out &
+if [ $? -ne 0 ]; then
+    cleanup 1
+fi
+sleep 1
+netcat -U $tmpdir/dnstap.sock < $DATAFILE
+sleep 1
+kill %1
+
+diff -i $OUTPUTFILE $tmpdir/debug2.out
 cleanup $?
