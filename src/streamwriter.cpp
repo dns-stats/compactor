882c8dc34dd87a6b03458b286debae2ae6fd8144
Sara Dickinson <sara@sinodun.com>
2022-01-26 14:53:15 +0000

    Changelog and copywrite updates fro 1.2.1-rc2

--- src/streamwriter.cpp
+++ src/streamwriter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 Internet Corporation for Assigned Names and Numbers.
+ * Copyright 2016-2018, 2022 Internet Corporation for Assigned Names and Numbers.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
